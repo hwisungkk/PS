a3bb4d1c236fd3ba15bab708e15f58844391be00
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2024-11-10 13:47:45 +0900

    [Silver I] Title: IOIOI, Time: 0 ms, Memory: 2236 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int ar[130][130]={0};
+int count1=0;
+int count2=0;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, b, c = 0, total = 0;
+    char ch;
+    int ma = -1000;
+    int count =0;
+    int n, m, z = 1;
+    int ar[101][101] = {0};
+    int check[1001];
+    vector<int> v1;
+    string s;
+    int dp[10000]={0};
+    cin >> n >> m;
+    cin >> s;
+    for(int i=0;i<m-2;i++){
+        if(s[i]=='I' && s[i+1]=='O' && s[i+2]=='I'){
+            dp[i] = dp[i-2]+1;
+            if(dp[i]>=n)count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
