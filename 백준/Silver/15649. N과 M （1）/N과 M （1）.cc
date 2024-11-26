3c197184690be6b8a08dbaad8d7016c5b2a8b4cb
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2024-11-26 12:21:34 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int al = 0;
+int print[9]={0};
+int check[9]={0};
+void back(int m, int n, int k){
+    for(int i=1;i<=m;i++){
+        if(k>n){
+            for(int p=1;p<=n;p++){
+                cout << print[p] << ' ';
+            }
+            cout << '\n';
+            return;
+        }
+        if(check[i]==0){
+            print[k]=i;
+            check[i]=1;
+            back(m, n, k+1);
+            check[i]=0;
+        }
+    }
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long a, b, c, total = 0;
+    long long count = 0;
+    int ma = 0;
+    long long n, k = -1;
+    int m;
+    string s1, s2;
+    cin >> m >> n;
+    back(m, n, 1);
+    
+    return 0;
+}
