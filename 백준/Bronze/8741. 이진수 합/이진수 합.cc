0dc7876576730128b86912cb7da698fa6c9775a2
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2024-11-30 12:50:06 +0900

    [Bronze II] Title: 이진수 합, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8741. 이진수 합/이진수 합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int al = 0;
+int main()
+{
+    ios_base ::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long a, b, c, total = 0;
+    long long n, k = -1;
+    int m;
+    string s1, s2;
+    int ar[3];
+    int max_dp[3]={0};
+    int min_dp[3]={0};
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cout<<'1';
+    }
+    
+    for(int i=1;i<n;i++){
+        cout<<'0';
+    }
+}
