a2edfd19e6637ff0b3e6a31b91ff515ec5274fc3
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2024-12-11 10:35:29 +0900

    [Silver III] Title: N과 M (7), Time: 428 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int ar[51][51]={0};
+int visited[51]={0};
+int k, m;
+vector <int> v;
+vector <int> pr;
+vector <int> check(10);
+void dfs(int n){
+    if(n==m){
+        for(auto a:pr)cout << a << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i=0;i<k;i++){
+        if(check[i]==0){
+            //check[i]=1;
+            pr.push_back(v[i]);
+            dfs(n+1);
+            check[i]=0;
+            pr.pop_back();
+        }
+    }
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long n;
+    long long a, b, c;
+
+    int t;
+    cin >> k >> m;
+    for(int i=0;i<k;i++){
+        cin >> c;
+        v.push_back(c);
+    }
+
+    sort(v.begin(), v.end());
+
+    dfs(0);
+
+    
+    
+    
+    return 0;
+}
