8f5d518cabb9047b91e46793852699895c36d1ab
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2024-12-12 11:45:46 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 200 ms, Memory: 25780 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+vector <int> vc;
+vector <int> pr;
+vector <int> check(8);
+int k, n;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int m;
+    int a, b, c;
+    cin >> n;
+    vector <int> v(n);
+    vector <int> pre;
+    vector <int> count(n);
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+    }
+    pre.push_back(v[0]);
+    for(int i=1;i<n;i++){
+        int l = -1;
+        int r = pre.size();
+        int mid = (l+r)/2;
+        while(l+1<r){
+            mid = (l+r)/2;
+            if(pre[mid] < v[i]) l = mid;
+            else r = mid;
+        }//lower bound
+        //cout << l+1 << ' ';
+        count[i] = l+1;
+        if(l+1<pre.size()){
+            pre[l+1] = v[i];
+        }
+        else{
+            pre.push_back(v[i]);
+        }
+    }//cout << '\n';
+    cout << pre.size() << '\n';
+    b = pre.size();
+    vector <int> ans(b);
+    b-=1;
+    for(int i=n-1;i>=0;i--){
+        if(count[i]==b){
+            ans.push_back(v[i]);
+            b--;
+        }
+        if(b==-1)break;
+    }
+    for(int i=0;i<pre.size();i++){
+        cout << ans.back() << ' ';
+        ans.pop_back();
+    }
+    return 0;
+}
