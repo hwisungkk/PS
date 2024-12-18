4b42907275491339b46e794ea718351ab769d714
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2024-12-18 13:58:19 +0900

    [Bronze IV] Title: A Simple Problem., Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long n, m, k;
+    long long a, b, c;
+
+    int t;
+    cin >> t;
+    while(t-->0){
+        cin >> n;
+        cout << n*n<<'\n';
+    }
+
+    return 0;
+}
