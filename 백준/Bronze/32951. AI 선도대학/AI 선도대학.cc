51a57834d36d7130787f692c34f7e5ce8e3fac23
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2024-12-23 20:03:21 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long n, m, k;
+    long long a, b, c;
+
+    int t;
+    cin >> t;
+    cout << t-2024;
+
+    return 0;
+}
