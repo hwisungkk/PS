212dd45ce0347bb8fbf96578ac55cf607c9c42f9
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2024-12-28 00:25:52 +0900

    [Bronze I] Title: Consonants (Small), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/12317. Consonants （Small）/Consonants （Small）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long n, m, k;
+    int a, b, c;
+
+    int t = 0;
+    cin >> t;
+    b=0;
+    while(t-->0){
+        b++;
+        c=0, m=0;
+        int count=0;
+        string s;
+        cin >> s >> a;
+        n = s.length();
+        vector <int> v(n,0);
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u')c++;
+            else c=0;
+            v[i]=c;
+
+            if(v[i]>=a){
+                count+= (i-a-m+2)*(n-i);
+                m = i-a+2;
+            }
+        }
+
+        cout << "Case #" << b << ": " << count << '\n';
+    }
+    return 0;
+}
