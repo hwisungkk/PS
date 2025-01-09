5f40d9a629d88c81a5d23a319eb8d678aaf21cc5
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-01-09 09:51:45 +0900

    [Silver V] Title: Flat Earth, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/19250. Flat Earth/Flat Earth.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    long long mi = 1e16;
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, b, c;
+    int n;
+    int t;
+    cin >> t;
+    while(t-->0){
+        int x, y, z, r, d;
+        cin >> x >> y >> z >> r >> a >> b >> c >> d;
+        cout << fixed;
+        cout.precision(9);
+        cout << r*r*3.1415926535<<'\n';
+    }
+
+    return 0;
+}
