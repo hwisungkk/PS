5e3366b3c723dac97eda4288e29cbec6b10addcc
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-01-13 21:56:46 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, m, k;
+    long long a, b, c, d;
+
+    int t;
+    cin >> t;
+    cout << t/5;
+    
+    
+    return 0;
+}
