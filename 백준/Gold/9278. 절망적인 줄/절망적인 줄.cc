da2f6a173c5fd6a5400c0637016ede4cacec7ec8
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-01-19 10:10:58 +0900

    [Gold IV] Title: 절망적인 줄, Time: 132 ms, Memory: 4128 KB -BaekjoonHub

--- 백준/Gold/9278. 절망적인 줄/절망적인 줄.cc
+++ 백준/Gold/9278. 절망적인 줄/절망적인 줄.cc
@@ -2,9 +2,9 @@
 #include <vector>
 #include <string>
 #include <tuple>
+#include <stack>
 #include <map>
 #include <set>
-#include <queue>
 #include <algorithm>
 #include <math.h>
 using namespace std;
@@ -13,29 +13,37 @@ int main()
     ios_base ::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    long long a, b, c;
+    int t;
+    int n, m, k;
+    int mi = 1e9;
     string s;
-    while (getline(cin, s)) {
-        int n = s.length();
-        int h = n/2;
-
-        vector<vector<int>> dp(n + 1, vector<int>(h + 1, 0));
-        
-        if (s[0] != ')') {
-            dp[0][1] = 1;
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= h; j++) {
-                if (j != 0 && s[i] != ')') {
-                    dp[i][j] = (dp[i-1][j-1] + dp[i][j]) % 1000000;
+    while(getline(cin, s)){
+        int total = s.size();
+        int t_50 = total/2;
+        vector <vector<int>> dp(total+1, vector<int>(t_50+1, 0));
+        if(s[0]!=')') dp[1][1]=1;//총 개수가 1개, 50원도 1개(여유분 느낌) '('
+        for(int i=1;i<s.size();i++){
+            if(s[i]==')'){
+                for(int j=1;j<=t_50;j++){
+                    dp[i+1][j-1] += dp[i][j]%1000000;
                 }
-                if (j != h && s[i] != '(') {
-                    dp[i][j] = (dp[i-1][j+1] + dp[i][j]) % 1000000;
+            }
+            else if(s[i]=='('){
+                for(int j=1;j<=t_50;j++){
+                    dp[i+1][j] += dp[i][j-1]%1000000;
+                }
+            }
+            else{
+                for(int j=1;j<=t_50;j++){
+                    dp[i+1][j] += dp[i][j-1]%1000000;
+                }
+                for(int j=1;j<=t_50;j++){
+                    dp[i+1][j-1] += dp[i][j]%1000000;
                 }
             }
         }
-        cout << dp[n-1][0] << '\n';
+        cout << dp[total][0]%1000000 << '\n';
     }
 
     return 0;
