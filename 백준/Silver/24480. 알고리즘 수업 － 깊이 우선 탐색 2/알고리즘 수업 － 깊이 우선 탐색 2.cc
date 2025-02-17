b0171b7668efe424548b30462a37a667ed218413
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-02-17 10:29:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 88 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+bool compare(int a, int b){
+    if(a>b)return 1;
+    else return 0;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b, c;
+    int n, m, t;
+    char c1, c2, c3;
+
+    cin >> n >> m >> t;
+    vector<vector<int>> v(n+1);
+    vector<int> ans(n+1,0);
+    vector<int> check(n+1,0);
+
+    for(int i=0;i<m;i++){
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    for(int i=1;i<=n;i++){
+        sort(v[i].begin(), v[i].end(), compare);
+    }
+    int cnt=1;
+    function <void(int)> dfs = [&](int n){
+        check[n]=1;
+        ans[n]=cnt;
+        for(int i=0;i<v[n].size();i++){
+            if(check[v[n][i]]==0){
+                cnt++;
+                dfs(v[n][i]);
+            }
+        }
+        
+    };
+    dfs(t);
+
+
+
+    for(int i=1;i<=n;i++){
+        cout << ans[i] << '\n';
+    }
+
+    return 0;
+}
