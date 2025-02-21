dd80028cf0ee00ef480a754efaa05bdd712777eb
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-02-21 22:41:01 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b, c;
+    int n, m, t;
+    cin >> n;
+    cout << 2000 << '\n';
+    for (int i = 0; i < 2000; i++) {
+        if(i<1000)cout << 1 << ' ';
+        else cout << 1000 << ' ';
+    }
+
+    return 0;
+}
