7732d6357e3dff394392494f0a1a36d57a642314
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-02-26 20:04:39 +0900

    [Platinum V] Title: 가짜 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/13319. 가짜 소수/가짜 소수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int dx[4] = {0,0,-1,1};
+int dy[4] = {1,-1,0,0};
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b, c, d;
+    long long n, m, t;
+    int total=0;
+    
+    cout << 601*1201*1801 << ' ' << 601;
+
+
+    return 0;
+}
