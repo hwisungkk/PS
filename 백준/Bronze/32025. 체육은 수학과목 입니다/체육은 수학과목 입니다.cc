d0abb88fb15d0725a005f369427998fe7e6d111d
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-03-08 08:43:05 +0900

    [Bronze IV] Title: 체육은 수학과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b, c, d;
+    int n, m, t;
+    cin >> n >> m;
+    long long ans=6;
+    
+    cout << min(n,m)*50;
+    
+    return 0;
+}
