f3a717ef13b997d4ff43a7931ef4f8be7d7822d0
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-03-07 14:15:28 +0900

    [Gold IV] Title: 데이터 만들기 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/7142. 데이터 만들기 3/데이터 만들기 3.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+#include <map>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b, c, d;
+    int n, m, k, t;
+    int o_1, o_2;
+    cout << 101 << '\n';
+    for(int i=0;i<101;i++){
+        cout << 0 << '\n';
+    }
+    cout << 1 << '\n';
+    cout << 0 << ' ' << 3 << '\n';
+
+    return 0;
+}
