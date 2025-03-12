90984ee3c72c0a3c7bf6af3989d8050079921d64
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-03-12 11:01:37 +0900

    [Gold V] Title: 회의실 배정, Time: 24 ms, Memory: 4196 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -1,47 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <functional>
+#include <queue>
 #include <stack>
-#include <map>
 #include <set>
+#include <functional>
 #include <algorithm>
 #include <math.h>
+#include <map>
 using namespace std;
-bool compare(pair<int, int> a, pair<int, int> b){
-    if(a.second<b.second)return 1;
-    else if(a.second==b.second){
-        if(a.first<b.first)return 1;
-        else return 0;
-    }
-    else return 0;
-}
 int main()
 {
     ios_base ::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-    int a, b, c;
+    
+    int a, b, c, d;
     int n, m, t;
-    char c1, c2, c3;
-
+    char ch;
+    
+    string s;
     cin >> n;
     vector<pair<int, int>> v;
     for(int i=0;i<n;i++){
         cin >> a >> b;
-        v.push_back({a,b});
+        v.push_back({b,a}); //끝, 시작
     }
-    sort(v.begin(), v.end(), compare);
-    int end = 0;
-    int cnt = 0;
+    sort(v.begin(), v.end());
+    priority_queue<int> pq;
+    pq.push(0);
+    int cnt=0;
     for(int i=0;i<n;i++){
-        if(v[i].first >= end){
-            end = v[i].second;
+        if(pq.top() <= v[i].second){
+            pq.push(v[i].first);
             cnt++;
         }
     }
     cout << cnt;
-
     return 0;
 }
