f91fab57458080f48807393556d5d3f597e7f24a
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-03-12 12:25:43 +0900

    [Silver II] Title: 나무 자르기, Time: 12 ms, Memory: 3012 KB -BaekjoonHub

---
+++ 백준/Silver/14247. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+#include <map>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b, c, d;
+    int n, m, t;
+    char ch;
+    
+    string s;
+    cin >> n;
+    vector<pair<long long, long long>> tree(n, {0,0});
+    for(int i=0;i<n;i++){
+        cin >> tree[i].second;
+    }
+    for(int i=0;i<n;i++){
+        cin >> tree[i].first;
+    }
+    sort(tree.begin(), tree.end());
+    long long ans=0;
+    for(int i=0;i<n;i++){
+        ans += (tree[i].second + tree[i].first*i);
+    }
+    cout << ans;
+    // vector<pair<int, int>> v;
+    // for(int i=0;i<n;i++){
+    //     cin >> a >> b;
+    //     v.push_back({b,a}); //끝, 시작
+    // }
+    // sort(v.begin(), v.end());
+    // priority_queue<int> pq;
+    // pq.push(0);
+    // int cnt=0;
+    // for(int i=0;i<n;i++){
+    //     if(pq.top() <= v[i].second){
+    //         pq.push(v[i].first);
+    //         cnt++;
+    //     }
+    // }
+    // cout << cnt;
+    return 0;
+}
