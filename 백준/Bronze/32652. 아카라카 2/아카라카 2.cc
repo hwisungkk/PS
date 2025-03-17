f0b0df02c474ce706ebb69c060a471c294c4dd9e
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-03-17 20:12:14 +0900

    [Bronze III] Title: 아카라카 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+#include <map>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b, c, d;
+    int n, m;
+    
+    cin >> n;
+    cout << "AKARAKA";
+
+    for(int i=0;i<n-1;i++){
+        cout << "RAKA";
+    }
+            
+    return 0;
+}
