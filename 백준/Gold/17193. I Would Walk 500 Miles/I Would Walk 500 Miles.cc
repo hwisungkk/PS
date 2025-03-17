d40869433502720f258a62c46d991f4dff5d95d1
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-03-17 19:33:52 +0900

    [Gold II] Title: I Would Walk 500 Miles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17193. I Would Walk 500 Miles/I Would Walk 500 Miles.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+#include <map>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b, c, d;
+    int n, m;
+    
+    cin >> n >> m;
+    cout << 2019201865 - 48*(n-1) -84*(m-2);
+    //long long li = (long long)(2019201913 * n + 2019201949 * m)%2019201997; //-84, -48
+            
+    return 0;
+}
