d9414a5f9a4d61e868461d92893830fd86b551a4
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-03-28 06:27:01 +0900

    [Gold II] Title: 돌무더기 게임 1, Time: 84 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/24678. 돌무더기 게임 1/돌무더기 게임 1.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+#include <map>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b, c, d;
+    int n, m, t;
+    char ch;
+    string s;
+    int ans=0;
+    bool flag=0;
+
+    long long aa, bb, cc;
+    cin >> t;
+    int cnt=0;
+    while(t-->0){
+        cnt=0;
+        cin >> a >> b >> c;
+        if(a%2==0)cnt++;
+        if(b%2==0)cnt++;
+        if(c%2==0)cnt++;
+
+        if(cnt>=2==0)cout << "B\n";
+        else cout << "R\n";
+
+    }
+    
+
+    return 0;
+}
