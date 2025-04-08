edae77e4e6e21df4a92a5d81cbf54c78b087bc47
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-04-08 14:39:10 +0900

    [Bronze II] Title: 비밀번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33702. 비밀번호/비밀번호.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+#include <map>
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b, c, d;
+    int n, m, t;
+    char ch;
+    string s;
+    bool flag=0;
+    cin >> n;
+
+    if(n%2)cout << "8";
+    else cout << "0";
+
+    return 0;
+}
