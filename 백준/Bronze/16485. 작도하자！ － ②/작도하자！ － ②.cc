f6c37ea8bf97e455054070dd72f44692a86d824a
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-06-21 23:47:32 +0900

    [Bronze II] Title: 작도하자! - ②, Time: 0 ms, Memory: 2020 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int n, m;
+int main()
+{
+    ios_base ::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> n >> m;
+    cout << (double)n/m;
+    
+    return 0;
+}
