8106e1795bba8a09705836c7e190b5b8efc9f957
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-09-24 09:00:42 +0900

    [Bronze V] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,24 @@
+#define ll long long
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,0,-1,1};
+int dy[4]={1,-1,0,0};
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long mod = 998'244'353;
+
+    long long a, b, c, d;
+    long long ans = 0;
+    using P = pair<int, int>;
+
+    cin >> n;
+    for(int i=0;i<n;i++)cout<<"Hello World, Judge " << i+1 << "!\n";
+    
+    return 0;
+}
