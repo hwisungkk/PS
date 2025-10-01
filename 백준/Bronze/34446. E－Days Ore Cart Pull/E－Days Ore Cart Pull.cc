fa624d5ba6a0f5d6a40bd8bb3b3360b19b3537c7
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-10-01 22:52:29 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.cc
@@ -0,0 +1,37 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,0,-1,1};
+int dy[4]={1,-1,0,0};
+int chk[100][100][100];
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long mod = 998'244'353;
+    using P=pair<int,int>;
+
+    long long a, b, c, d;
+    long long ans = 0;
+    string s;
+
+    cin >> a >> b >> c;
+    cout << 2*a;
+
+    return 0;
+}
