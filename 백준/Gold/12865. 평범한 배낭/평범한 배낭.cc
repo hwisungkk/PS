bbe42e8001074c8a608f20016a7ab7ce1bb32dcf
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-11-08 19:35:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2968 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,33 +1,67 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
 using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,0,-1,1};
+int dy[4]={1,-1,0,0};
+int chk[100][100][4];
+int parent[1001];
+int ran[1001];
+int find(int u) {
+	if (u == parent[u]) return u;
+	return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+	u = find(u);
+	v = find(v);
+	if (u == v) return;
+	if (ran[u] > ran[v]) swap(u, v);
+	parent[u] = v;
+	if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+struct fire{
+    int r;
+    int c;
+    int mass;
+    int speed;
+    int dir;
+};
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m;
-    int a, b;
-    vector <pair<int,int>> v;
-    int dp[200001] = {0};
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long mod = 1'000'000'007;
+    using P=pair<ll,ll>;
+
+    long long a, b, c, d;
+    long long ans = 0;
+
     cin >> n >> m;
+
+    vector<ll> dp(m+1, 0);
+
     for(int i=0;i<n;i++){
         cin >> a >> b;
-        v.push_back({a,b});
-    }
-    dp[0] = 1;
-    for(int j=0;j<n;j++){
-        for(int i=100000;i>=0;i--){
-            if(dp[i]!=0){
-                if(dp[i+v[j].first] < dp[i]+v[j].second)
-                    dp[i+v[j].first] = dp[i]+v[j].second;
-            }
+        for(int j=m;j>=a;j--){
+            dp[j]=max(dp[j-a]+b, dp[j]);
         }
     }
-    for(int i=1;i<=m;i++){
-        if(dp[i-1]>dp[i]){
-            dp[i] = dp[i-1];
-        }
-    }
-    cout << dp[m]-1 <<'\n';
+    for(int i=0;i<=m;i++)ans=max(ans, dp[i]);
+    cout << ans;
+
+    return 0;
 }
