5d0c6fbfc1e6f4d2b46c9ea6bf57e4916b4db456
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-11-30 16:33:09 +0900

    [Platinum IV] Title: 님블, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Platinum/11869. 님블/님블.cc
@@ -0,0 +1,65 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,0,-1,1};
+int dy[4]={1,-1,0,0};
+int chk[100][100][4];
+int parent[1001];
+int ran[1001];
+int find(int u) {
+   if (u == parent[u]) return u;
+   return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+   u = find(u);
+   v = find(v);
+   if (u == v) return;
+   if (ran[u] > ran[v]) swap(u, v);
+   parent[u] = v;
+   if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+struct fire{
+    int r;
+    int c;
+    int mass;
+    int speed;
+    int dir;
+};
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long mod = 1e9+7;
+        
+    bool flag=0; 
+    long long a, b, c, d;
+    long long n, m, t, k;
+    long long ans = 0;
+    ll total=0;
+
+    cin >> n;
+    ll dv=0;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        dv^=a;
+    }
+    if(dv==0)cout<<"cubelover";
+    else cout<<"koosaga";
+
+    return 0;
+}
