ac7ce3a440b2f6a8af3cf3d59c5a5c104cd3fd08
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2025-12-12 13:49:53 +0900

    [Silver II] Title: 상품 is 뭔들, Time: 940 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Silver/12779. 상품 is 뭔들/상품 is 뭔들.cc
@@ -0,0 +1,67 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,0,-1,1};
+int dy[4]={1,-1,0,0};
+int chk[100][100][4];
+int parent[1001];
+int ran[1001];
+int find(int u) {
+   if (u == parent[u]) return u;
+   return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+   u = find(u);
+   v = find(v);
+   if (u == v) return;
+   if (ran[u] > ran[v]) swap(u, v);
+   parent[u] = v;
+   if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    using P=pair<ll,ll>;
+
+    long long mod = 1e9+7;
+        
+    bool flag=0; 
+    long long a, b, c, d;
+    long long n, m, t, k;
+    long long ans = 0;
+
+    cin >> n >> m;
+
+    ll bunmo=m-n;
+    ll bunja=0;
+
+    for(long long i=1;;i++){
+        long long i2=i*i;
+        if(i2>m)break;
+        if(i2>n)bunja++;
+    }
+
+    ll tong=gcd(bunja, bunmo);
+
+    if(bunja==0)cout<<0;
+    else cout<< bunja/tong << '/' << bunmo/tong;
+
+    return 0;
+}
+
+//9223372036854775808
