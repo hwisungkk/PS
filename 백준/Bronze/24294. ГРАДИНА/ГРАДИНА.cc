f35e0e89ffb44c3cc6b770a65c0a6e1d81ae5344
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2026-01-30 14:29:12 +0900

    [Bronze IV] Title: ГРАДИНА, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Bronze/24294. ГРАДИНА/ГРАДИНА.cc
@@ -0,0 +1,58 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,0,-1,1};
+int dy[4]={1,-1,0,0};
+int chk[100][100][4];
+int parent[1001];
+int ran[1001];
+int find(int u) {
+   if (u == parent[u]) return u;
+   return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+   u = find(u);
+   v = find(v);
+   if (u == v) return;
+   if (ran[u] > ran[v]) swap(u, v); 
+   parent[u] = v;                                                                                                                                                                                                        
+   if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    using P=pair<ll,ll>;
+
+    long long mod = 1e9+7;
+        
+    bool flag=0; 
+    long long a, b, c, d;
+    long long n, m, t, k;
+    long long ans = 0;
+
+    cin >> a >> b >> c >> d;
+    ans+=2*b;
+    ans+=(a+2);
+    ans+=2*d;
+    ans+=(c+2);
+    ans+=(abs(a-c));
+
+    cout << ans;
+
+    return 0;
+}
