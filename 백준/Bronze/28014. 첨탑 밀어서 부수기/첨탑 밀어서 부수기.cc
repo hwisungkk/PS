f932255662015ee98949806640e7f5f8416eeb9f
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2026-02-18 21:26:22 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 300 ms, Memory: 21716 KB -BaekjoonHub

---
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.cc
@@ -0,0 +1,60 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,0,-1,1};
+int dy[4]={1,-1,0,0};
+int chk[100][100][4];
+int parent[1001];
+int ran[1001];
+const double PI = acos(-1.0);
+int find(int u) {
+   if (u == parent[u]) return u;
+   return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+   u = find(u);
+   v = find(v);
+   if (u == v) return;
+   if (ran[u] > ran[v]) swap(u, v);
+   parent[u] = v;
+   if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    using P=pair<ll,ll>;
+
+    long long mod = 1e9+7;
+
+    bool flag=0; 
+    long long a, b, c, d;
+    long long n, m, t, k;
+    long long ans=0;
+
+    cin >> n;
+    m=-1;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+        if(m<=v[i])ans++;
+        m=v[i];
+    }
+    cout << ans;
+
+    return 0;
+}
