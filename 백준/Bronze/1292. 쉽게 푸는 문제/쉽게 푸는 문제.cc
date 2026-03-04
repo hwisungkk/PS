c989049b4948c3d875c5e6f25bbd05dc53bab27a
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2026-03-04 13:16:33 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 0 ms, Memory: 2320 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
@@ -0,0 +1,67 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,0,-1,1};
+int dy[4]={1,-1,0,0};
+int chk[100][100][4];
+int parent[1001];
+int ran[1001];
+const double PI = acos(-1.0);
+int find(int u) {
+   if (u == parent[u]) return u;
+   return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+   u = find(u);
+   v = find(v);
+   if (u == v) return;
+   if (ran[u] > ran[v]) swap(u, v);
+   parent[u] = v;
+   if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    using P=pair<ll,ll>;
+
+    long long mod = 1e9+7;
+
+    bool flag=0; 
+    long long a, b, c, d;
+    long long n, m, t, k;
+    long long ans=0;
+
+    cin >> n >> m;
+    vector<int> v;
+
+    c=1;
+    while(v.size()<=1000){
+        for(int i=0;i<c;i++){
+            v.push_back(c);
+        }
+        c++;
+    }
+
+    ll tt=0;
+    for(int i=n-1;i<=m-1;i++){
+        tt+=v[i];
+    }
+    cout<<tt;
+
+    return 0;
+}
