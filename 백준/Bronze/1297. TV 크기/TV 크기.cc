5f3819f44876127eb750d711c37ee384f49ffdfd
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2026-03-04 13:17:25 +0900

    [Bronze II] Title: TV 크기, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Bronze/1297. TV 크기/TV 크기.cc
@@ -0,0 +1,58 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+int parent[1001];
+int ran[1001];
+const double PI = acos(-1.0);
+int find(int u) {
+   if (u == parent[u]) return u;
+   return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+   u = find(u);
+   v = find(v);
+   if (u == v) return;
+   if (ran[u] > ran[v]) swap(u, v);
+   parent[u] = v;
+   if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    using P=pair<ll,ll>;
+
+    long long mod = 1e9+7;
+        
+    bool flag=1;
+    long long a, b, c, d;
+    long long n, m, t, k;
+    long long ans=0;
+
+    cin >> n >> m >> t;
+
+    double dd = sqrt(1.0 * m * m + 1.0 * t * t);
+    double kk = n/dd;
+
+    int realm = (int)floor(kk * m);
+    int realt = (int)floor(kk * t);
+
+    cout << realm << ' ' << realt << "\n";
+    return 0;
+}
