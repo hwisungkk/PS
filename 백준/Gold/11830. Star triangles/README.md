75a7626919534383c29130fc9d48a89c0bbac397
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2026-03-04 13:15:18 +0900

    [Gold V] Title: Star triangles, Time: 396 ms, Memory: 9892 KB -BaekjoonHub

---
+++ 백준/Gold/11830. Star triangles/README.md
@@ -0,0 +1,28 @@
+# [Gold V] Star triangles - 11830 
+
+[문제 링크](https://www.acmicpc.net/problem/11830) 
+
+### 성능 요약
+
+메모리: 9892 KB, 시간: 396 ms
+
+### 분류
+
+수학, 자료 구조, 기하학, 집합과 맵, 조합론, 해시를 사용한 집합과 맵
+
+### 제출 일자
+
+2026년 3월 3일 15:31:39
+
+### 문제 설명
+
+<p>Zhomart likes watching the stars and construct a variety of geometric shapes from them. The sky is represented in the form of the Cartesian coordinate system, and the stars are represented by points on it. At this moment Zhomart interested in the question: how many different right triangles whose legs are parallel to the axes, you can create with the help of stars in the sky.</p>
+
+### 입력 
+
+ <p>In the first line of the input line you are given N –– the number of stars on the sky (3 ≤ N ≤ 300000). Each of the next N lines contains integer X and Y (|X, Y| ≤ 10<sup>9</sup>) –– coordinates of the appropriate star.</p>
+
+### 출력 
+
+ <p>Print one number – the answer to the question.</p>
+
---
+++ 백준/Gold/11830. Star triangles/Star triangles.cc
@@ -0,0 +1,68 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+int parent[1001];
+int ran[1001];
+const double PI = acos(-1.0);
+int find(int u) {
+   if (u == parent[u]) return u;
+   return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+   u = find(u);
+   v = find(v);
+   if (u == v) return;
+   if (ran[u] > ran[v]) swap(u, v);
+   parent[u] = v;
+   if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    using P=pair<ll,ll>;
+
+    long long mod = 1e9+7;
+        
+    bool flag=1; 
+    long long a, b, c, d;
+    long long n, m, t, k;
+    long long ans=0;
+
+    string s;
+    cin >> n;
+    vector<P> vp(n);
+    map<int, int> garo;
+    map<int, int> sero;
+    for(int i=0;i<n;i++){
+        cin >> a >> b;
+        vp[i]={a,b};
+        if(garo.find(a)==garo.end())garo[a]=1;
+        else garo[a]++;
+
+        if(sero.find(b)==sero.end())sero[b]=1;
+        else sero[b]++;
+    }
+    for(int i=0;i<n;i++){
+        ans+=(garo[vp[i].first]-1)*(sero[vp[i].second]-1);
+    }
+    cout<<ans;
+
+    return 0;
+}
