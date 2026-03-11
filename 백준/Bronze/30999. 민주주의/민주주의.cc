a0ea457c85632554c3fc84f1265b2c3520950c36
HS_Tw <50361172+hwisungkk@users.noreply.github.com>
2026-03-11 10:45:43 +0900

    [Bronze IV] Title: 민주주의, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Bronze/30999. 민주주의/민주주의.cc
@@ -0,0 +1,61 @@
+#define ll long long
+#define i128 __int128_t
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m, t, k = 0;
+bool flag = 0;
+int parent[1001];
+int ran[1001];
+const double PI = acos(-1.0);
+int find(int u) {
+   if (u == parent[u]) return u;
+   return parent[u] = find(parent[u]);
+}
+void uni(int u, int v) {
+   u = find(u);
+   v = find(v);
+   if (u == v) return;
+   if (ran[u] > ran[v]) swap(u, v);
+   parent[u] = v;
+   if (ran[u] == ran[v]) ran[v]++;
+}
+ll gcd(ll x, ll y){
+    if(x<y)swap(x,y);
+
+    while(y!=0){
+        ll tmp=y;
+        y=x%y;
+        x=tmp;
+    }
+    return x;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    using P=pair<ll,ll>;
+
+    long long mod = 1e9+7;
+        
+    bool flag=0; 
+    long long a, b, c, d;
+    long long n, m, t, k;
+    long long ans=0;
+
+    cin >> n >> m;
+    vector<string> vs(n);
+    for(int i=0;i<n;i++){
+        cin >> vs[i];
+
+        a=0,b=0;
+        for(int j=0;j<m;j++){
+            if(vs[i][j]=='O')a++;
+            else b++;
+        }
+        if(a>b)ans++;
+    }
+    cout << ans;
+
+    return 0;
+}
